09ee9e7cef0eb87d8c2938ab90435d141d105851
cynthia <nwekecynthia2015@gmail.com>
2023-12-12 09:11:15 -0800

    intial commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,58 +1,58 @@
-import NextAuth from "next-auth/next";
-import  CredentialsProvider  from "next-auth/providers/credentials";
-import connectDB from "../../../../utils/mongodb"
-import User from '../../../../models/user' 
-import bcrypt from "bcryptjs"
+// import NextAuth from "next-auth/next";
+// import  CredentialsProvider  from "next-auth/providers/credentials";
+// import connectDB from "../../../../utils/mongodb"
+// import User from '../../../../models/user' 
+// import bcrypt from "bcryptjs"
 
-export const authOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "credentials",
-            credential:{},
+// export const authOptions = {
+//     providers: [
+//         CredentialsProvider({
+//             name: "credentials",
+//             credential:{},
                 
 
 
            
 
-            async authorize(credentials) {
-                // const user = { id: "1"}
-                // return user;
+//             async authorize(credentials) {
+//                 // const user = { id: "1"}
+//                 // return user;
                 
                 
-                const {email, password} = credentials
+//                 const {email, password} = credentials
 
-                try {
-                    await connectDB()
+//                 try {
+//                     await connectDB()
                     
-                    const user = await User.findOne({email})
+//                     const user = await User.findOne({email})
 
-                    if(!user){
-                        return null; 
-                    }  
-                    const passwordMatch = await bcrypt.compare(password, user.password)
+//                     if(!user){
+//                         return null; 
+//                     }  
+//                     const passwordMatch = await bcrypt.compare(password, user.password)
 
 
-                        if(!passwordMatch){
-                            return null;
-                        }  
-                        return user;              
-                    } catch (error) {
-                        console.log("Error: ", error);
+//                         if(!passwordMatch){
+//                             return null;
+//                         }  
+//                         return user;              
+//                     } catch (error) {
+//                         console.log("Error: ", error);
                         
-                    }
+//                     }
                 
-            },
+//             },
 
-        }),
-    ],
-    session:{
-        strategy:"jwt",
-    },
-    secret: process.env.NEXT_SECRET,
-    pages:{
-        signIn:"/"
-    },
-};
-const handler = NextAuth(authOptions)
+//         }),
+//     ],
+//     session:{
+//         strategy:"jwt",
+//     },
+//     secret: process.env.NEXT_SECRET,
+//     pages:{
+//         signIn:"/"
+//     },
+// };
+// const handler = NextAuth(authOptions)
 
-export {handler as GET, handler as POST}
+// export {handler as GET, handler as POST}
