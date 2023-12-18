51fa805e2c504d2e67080470482a6d1294148e09
cynthia <nwekecynthia2015@gmail.com>
2023-12-17 16:36:15 -0800

    intial commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,8 +9,8 @@ export const authOptions = {
         CredentialsProvider({
             name: "Credentials",
             credentials:{
-                // email: { label: "Email", type: "text" },
-                // password: { label: "Password", type: "password" }
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" }
             },
         
             async authorize(credentials) {
